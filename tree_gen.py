7cbe86ef1145be4f430aa129927c0f615c39fa02
avicooper <avicooper007@gmail.com>
2020-05-01 11:48:18 -0400

    Added code for SPINN, and started working on tree generation for large datasets

--- tree_gen.py
+++ tree_gen.py
@@ -1,5 +1,6 @@
 from stanfordcorenlp import StanfordCoreNLP
 import logging
+import tools
 
 def gen_tikz_qtree(parse):
     parse = parse.replace('(', '[.')
@@ -10,10 +11,18 @@ def gen_tikz_qtree(parse):
 if __name__ == '__main__':
     sNLP = StanfordCoreNLP('stanford-corenlp-full-2020-04-20', port=9000, memory='8g', logging_level=logging.DEBUG)
     text = 'I went to the nearby makolet and bought some yellow potatoes.'
-    parse_tree = gen_tikz_qtree(sNLP.parse(text))
-    tex_code = ''
-    with open('tex_files/tree_vis.tex', 'r') as f:
-        tex_code = f.read()
-    with open('tex_files/parsed_tree_vis.tex', 'w') as f:
-        f.write(tex_code.replace('TREE', parse_tree))
-    sNLP.close()
+    batches = tools.load_wikitext_103(128)
+    for batch in batches:
+        parse_trees = sNLP.parse(text)
+        with open('test.txt', 'a') as f:
+            f.write(parse_trees)
+        break
+
+
+
+    # tex_code = ''
+    # with open('tex_files/tree_vis.tex', 'r') as f:
+    #     tex_code = f.read()
+    # with open('tex_files/parsed_tree_vis.tex', 'w') as f:
+    #     f.write(tex_code.replace('TREE', parse_tree))
+    # sNLP.close()
