970ba5a88639e05a267a379162eb4fcaae3665d7
avicooper <avicooper007@gmail.com>
2020-05-05 12:31:19 -0400

    updated scripts

---
+++ submit_experiment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=my_job
+#SBATCH --ntasks=1 --nodes=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --gres=gpu:p100:1
+#SBATCH --time=00:20:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=avi.cooper@yale.edu
+
+./run_experiment.sh
