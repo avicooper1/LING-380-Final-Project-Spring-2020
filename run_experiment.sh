ac5efd880f3465abb2dfd7496b69271f6e2a9fb1
avicooper <avicooper007@gmail.com>
2020-05-05 16:05:38 -0400

    updated to run 4 experiments in parallel

--- run_experiment.sh
+++ run_experiment.sh
@@ -6,3 +6,5 @@ conda activate fpenv
 nvidia-smi
 python example_lm.py --epochs 50 --model SRN --glove_set 6B --device cuda:0 > ../SRN_output.txt 2>&1 &
 python example_lm.py --epochs 50 --model LSTM --glove_set 6B --device cuda:1 > ../LSTM_output.txt 2>&1 &
+python example_lm.py --epochs 50 --model GRU --glove_set 6B --device cuda:2 > ../GRU_output.txt 2>&1 &
+python example_lm.py --epochs 50 --model SPINN --glove_set 6B --device cuda:3 > ../SPINN_output.txt 2>&1 &
