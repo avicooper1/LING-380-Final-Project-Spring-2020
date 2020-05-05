135eff909050817ca332783e3e57209ed0aa248a
avicooper <avicooper007@gmail.com>
2020-05-05 19:26:08 -0400

    flush every line

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,7 +4,7 @@ module load miniconda
 source ../.bash_profile
 conda activate fpenv
 nvidia-smi
-python example_lm.py --epochs 50 --model SRN --glove_set 6B --device cuda:0 >> ../SRN2_output.txt
+stdbuf -oL python example_lm.py --epochs 50 --model SRN --glove_set 6B --device cuda:0 > ../SRN3_output.txt
 #python example_lm.py --epochs 50 --model LSTM --glove_set 6B --device cuda:0 >> ../LSTM2_output.txt
 #python example_lm.py --epochs 50 --model GRU --glove_set 6B --device cuda:0 >> ../GRU2_output.txt
 #python example_lm.py --epochs 50 --model SPINN --glove_set 6B --device cuda:0 >> ../SPINN2_output.txt
