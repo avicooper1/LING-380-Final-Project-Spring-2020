5f69f205821e1e433039c92aeae30886607f9ab8
avicooper <avicooper007@gmail.com>
2020-05-05 14:09:13 -0400

    experiment changes

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,4 +4,4 @@ module load miniconda
 source ../.bash_profile
 conda activate fpenv
 nvidia-smi
-python example_lm.py --epochs 1 --model SPINN --glove_set 6B
+python example_lm.py --epochs 50 --model SRN --glove_set 6B
